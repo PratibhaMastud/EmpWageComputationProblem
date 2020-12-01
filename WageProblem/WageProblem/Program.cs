47e569ccbc6e71e27cb1eeeca8ae8c766f65266a
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-01 17:49:47 +0530

    [Pratibha Mastud] Add: Find Daily Employee Wage

--- WageProblem/WageProblem/Program.cs
+++ WageProblem/WageProblem/Program.cs
@@ -7,17 +7,16 @@ namespace WageProblem
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Welcome Employee Wage Computation");
-			checkEmployeeAttendence();
+			dailyEmployeeWage();
 		}
 
-		public static void checkEmployeeAttendence()
+		public static int dailyEmployeeWage()
 		{
-			Random rd = new Random();
-			int rand_num = rd.Next(0, 1);
-			if (rand_num == 1)
-				Console.WriteLine("Employee is Present");
-			else
-				Console.WriteLine("Employee is Not Present");
+			int wagePerHour = 20;
+			int dayHour = 8;
+			int dailyEmployeeWage = wagePerHour * dayHour;
+			Console.WriteLine("Daily Employee Wage is : {0}", dailyEmployeeWage);
+			return dailyEmployeeWage;
 		}
 	}
 }
