188b383289424ef030b23b38814ab739693523d5
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-01 18:20:40 +0530

    Added: Find Daily Wage Employee

---
+++ EmpWage/EmpWage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EmpWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            dailyEmployeeWage();
+        }
+        public static int dailyEmployeeWage()
+        {
+            int wagePerHour = 20;
+            int dayHour = 8;
+            int dailyEmployeeWage = wagePerHour * dayHour;
+            Console.WriteLine("Daily Employee Wage is : {0}", dailyEmployeeWage);
+            return dailyEmployeeWage;
+        }
+    }
+}
